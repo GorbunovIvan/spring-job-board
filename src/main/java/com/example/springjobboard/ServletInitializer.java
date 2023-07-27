cb0c6055163ba1f8285662be8eaa0b562ffa0865
GorbunovIvan <gorbunovvivan@gmail.com>
2023-07-27 09:03:12 +0300

    init

---
+++ src/main/java/com/example/springjobboard/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springjobboard;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringJobBoardApplication.class);
+	}
+
+}
---
+++ src/main/java/com/example/springjobboard/SpringJobBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJobBoardApplication.class, args);
+	}
+
+}
